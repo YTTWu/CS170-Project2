06f28cfd1f467ea6e32768554e660bc7dbe5443c
Yuteng Terrence Wu <yutengwu@yahoo.com>
2022-12-02 00:05:42 -0800

    everything works but need to make it prettier

--- FeatureSelection.py
+++ FeatureSelection.py
@@ -1,4 +1,4 @@
-import csv
+import time
 
 class FeatureSelection:
     def __init__(self, data):
@@ -26,7 +26,7 @@ class FeatureSelection:
                 if self.data[i][0] == self.data[minDis[1]][0]:
                     count += 1
             minDis = [999,0]
-        # print("Accuracy: ", count/len(self.data))
+        # print("Using feature { ", count/len(self.data))
         return count / len(self.data)
 
     
@@ -46,6 +46,7 @@ class FeatureSelection:
                 if i not in temp_feature:
                     temp_feature.append(i)
                     temp.append([self.getAccuracy(temp_feature),temp_feature.copy()])
+                    print("Using feature " + str(temp_feature) + " accuracy is " + str(temp[-1][0]))
                     temp_feature.pop()
             curBestAccuracy = sorted(temp, key=lambda x: x[0], reverse=True)
             f = curBestAccuracy[0][1]
@@ -57,18 +58,55 @@ class FeatureSelection:
         result = sorted(totalBest, key=lambda x: x[0], reverse=True)[0]
         return result
 
-
-
+    def backwardElimination(self):
+        totalBest = []
+        temp_feature = []
+        curBestAccuracy = []
+        temp = []
+        initial = []
+        for k in range(1,len(self.data[0])):
+            temp_feature.append(k)
+            initial.append(k)
+        #maybe need to run one time for all before enter the loop
+        temp.append([self.getAccuracy(temp_feature),temp_feature.copy()])
+        print("Using feature " + str(initial) + " accuracy is " + str(temp[0][0]))
+        totalBest.append(temp[0])
+        while len(temp_feature) != 0:
+            for i in reversed(range(len(self.data[0]))):
+                if i in temp_feature:
+                    temp_feature.remove(i)
+                    temp.append([self.getAccuracy(temp_feature),temp_feature.copy()])                    
+                    print("Using feature " + str(temp_feature) + " accuracy is " + str(temp[-1][0]))
+                    temp_feature.append(i)
+            curBestAccuracy = sorted(temp, key=lambda x: x[0], reverse=True)
+            b = curBestAccuracy[0][1]
+            temp_feature = []
+            if len(b) == 1:
+                temp_feature = []
+            else:
+                for x in b:
+                    if x not in temp_feature:
+                        temp_feature.append(x)
+            totalBest.append(curBestAccuracy[0])
+            temp = []
+        result = sorted(totalBest, key=lambda x: x[0], reverse=True)[0]
+        return result
 
             
 
 def main():
     data = []
-    with open("P2_datasets/CS170_Small_Data__107.txt", "r") as file:
+    with open("P2_datasets/CS170_Large_Data__124.txt", "r") as file:
         data = [[x for x in line.split()] for line in file]
-    print(data)
+    # print(data)
     F = FeatureSelection(data)
-    print(F.forwardSelection())
+    startTime = time.time()
+    print("FORWARD: ", F.forwardSelection())
+    endtime = time.time()
+    totalTime = endtime - startTime
+    print("Time: ", totalTime)
+    # print("BACKWORD: ", F.backwardElimination())
+    
     
 
 main()
